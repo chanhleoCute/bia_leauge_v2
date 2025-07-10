16701fcf2e2112717faba0b27d10843e63fd0c48
chanhleoCute <thinhxom1109@gmail.com>
2025-07-10 23:25:45 +0700

    Update sheet.py

--- sheets.py
+++ sheets.py
@@ -17,12 +17,20 @@ sheet = client.open(SHEET_NAME).sheet1
 def load_players():
     records = sheet.get_all_records()
     players = []
-    for r in records:
-        name = r['name']
-        rank = Rank.from_str(r['rank'])
-        points = int(r['points'])
-        players.append(Player(name, rank, points))
+
+    for i, r in enumerate(records):
+        try:
+            name = r['name']
+            rank = Rank.from_str(r['rank'])
+            points = int(r['points'])
+            players.append(Player(name, rank, points))
+        except KeyError as e:
+            print(f"Dòng {i+2} thiếu khóa: {e} - Giá trị r: {r}")
+        except Exception as e:
+            print(f"Dòng {i+2} lỗi không xác định: {e} - Giá trị r: {r}")
+
     return players
+
 
 def save_players(players):
     sheet.clear()
